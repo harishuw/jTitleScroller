b5eefd19b09cef03f7182464ff00aab80db98b29
Harish U Warrier <harishuw@gmail.com>
2014-11-23 21:50:12 +0530

    Stop Function

--- js/titleScroller.js
+++ js/titleScroller.js
@@ -1,33 +1,36 @@
-function setTitle()
-   {
-		var title=document.title;
-		var tmp_title=title;
-		var titletimer;
-		var speed=200;
-		var interval=800;
-		if(document.getElementsByTagName("title")[0].hasAttribute('data-speed'))
-			speed=document.getElementsByTagName("title")[0].getAttribute('data-speed');
-			
-		if(document.getElementsByTagName("title")[0].hasAttribute('data-interval'))
-			interval=document.getElementsByTagName("title")[0].getAttribute('data-interval');	
-			
-			speed=isNaN(speed)?200:speed;	
-			interval=isNaN(interval)?800:interval;	
-			
-		setTimeout(function(){
-			titletimer=setInterval(function(){
-			tmp_title=tmp_title.substr(1, tmp_title.length-1)
-			document.title=tmp_title
-			if(tmp_title.length==1)
-			{
-			tmp_title=title;
-			document.title=title
-			clearInterval(titletimer);
-				setTitle();
-				
-			}
-		},speed);		
-				
-	},interval);
-   }
-   setTitle();
+var titletimer;
+var orig_title=document.title;
+function scrollTitle()
+{
+     var title=document.title;
+     var tmp_title=title;
+     var speed=200;
+     var interval=800;
+     if(document.getElementsByTagName("title")[0].hasAttribute('data-speed'))
+         speed=document.getElementsByTagName("title")[0].getAttribute('data-speed');
+     if(document.getElementsByTagName("title")[0].hasAttribute('data-interval'))
+         interval=document.getElementsByTagName("title")[0].getAttribute('data-interval');	
+     speed=isNaN(speed)?200:speed;	
+     interval=isNaN(interval)?800:interval;	
+
+     setTimeout(function(){
+             titletimer=setInterval(function(){
+                 tmp_title=tmp_title.substr(1, tmp_title.length-1)
+                 document.title=tmp_title
+                 if(tmp_title.length==1)
+                 {
+                     tmp_title=title;
+                     document.title=title
+                     clearInterval(titletimer);
+                     scrollTitle();
+                 }
+             },speed);		
+
+     },interval);
+}
+scrollTitle();
+function stopScroll()
+{
+    clearInterval(titletimer);
+    document.title=orig_title;
+}
